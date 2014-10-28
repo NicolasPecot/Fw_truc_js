30f382426e8b92aedbd3b1caf665247ba66fad26
Nicolas Pécot <nicolas.pecot@gmail.com>
2014-10-28 20:41:05 +0100

    Modification of app.js added code to manage routing

--- app.js
+++ app.js
@@ -13,4 +13,18 @@ var paths = {
     logs:       this.ROOT + '/app/logs',
     controllers:this.ROOT + '/src/controllers',
     models:     this.ROOT + '/src/models'
-}
+};
+
+var routes = require(paths.config + 'routes.json');
+
+exports.start = function(request, response, next){
+    var url = require('url');
+    var querystring = require('querystring');
+
+    var path = url.parse(request.url).pathname;
+    var page = $.require('router').get(routes, path);
+    console.log(page);
+
+    reponse.writeHead(200, {'Content-Type':'text/html'});
+    response.end('hello world index');
+};
